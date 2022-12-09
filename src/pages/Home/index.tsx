d0b784cb50b99222d72b4e3880ef25cb6f01cde1
joaomlg <joaomarcos1999jm@gmail.com>
2022-12-09 19:04:33 -0300

    Home page static sketch

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Button } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
-import { Container } from './styles';
+import { AccountInfo, AccountSection, Container, Header, ManageAccountButtonContainer, MonthSelector, UpdatedAt } from './styles';
 
 const Home: React.FC = () => {
   return (
     <Container>
       <StatusBar style="auto" />
-      <Text>Home</Text>
+      <Header>
+        <MonthSelector>
+          <Text>Maio</Text>
+          <MaterialIcons name="expand-more" />
+        </MonthSelector>
+        <MaterialIcons name="refresh" />
+      </Header>
+      <UpdatedAt>Atualizado em: 09/12/2022 - 17:00</UpdatedAt>
+      <AccountSection>
+        <AccountInfo>
+          <Text>Saldo das contas</Text>
+          <Text>R$ 764</Text>
+        </AccountInfo>
+        <AccountInfo>
+          <Text>Fatura dos cartões</Text>
+          <Text>-R$ 322</Text>
+        </AccountInfo>
+        <AccountInfo>
+          <Text>Investimentos</Text>
+          <Text>R$ 1.903,32</Text>
+        </AccountInfo>
+        <AccountInfo>
+          <Text>Total</Text>
+          <Text>2.345,32</Text>
+        </AccountInfo>
+      </AccountSection>
+      <ManageAccountButtonContainer>
+        <Button title='Gerenciar contas' />
+      </ManageAccountButtonContainer>
     </Container>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,7 +2,35 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  background-color: ${({ theme }) => theme.COLORS.BACKGROUND};
+  background-color: #fff;
+  padding: 24px 12px;
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
+`;
+
+export const MonthSelector = styled.View`
+  flex-direction: row;
+  align-items: center;
+  gap: 12px;
+`;
+
+export const UpdatedAt = styled.Text`
+  padding: 12px 0;
+`;
+
+export const AccountSection = styled.View`
+
+`;
+
+export const AccountInfo = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const ManageAccountButtonContainer = styled.View`
+  padding: 12px 0;
 `;
