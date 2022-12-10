5c9791388130d4b7af7c71eb2a8606b58f8f66f6
joaomlg <joaomarcos1999jm@gmail.com>
2022-12-10 14:26:16 -0300

    pluggy-sdk removed

--- src/services/pluggy/client.ts
+++
@@ -1,9 +0,0 @@
-import { PluggyClient } from 'pluggy-sdk';
-import { CLIENT_ID, CLIENT_SECRET } from '@env';
-
-const client = new PluggyClient({
-  clientId: CLIENT_ID,
-  clientSecret: CLIENT_SECRET,
-})
-
-export default client;
