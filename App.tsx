7f946a231757959397de424eba79cc6fd34ec2bf
joaomlg <joaomarcos1999jm@gmail.com>
2022-12-17 13:08:45 -0300

    native-base ui lib added

--- App.tsx
+++ App.tsx
@@ -4,14 +4,17 @@ import { ThemeProvider } from 'styled-components/native';
 import HooksProvider from './src/hooks';
 import Routes from './src/routes';
 import light from './src/theme/light';
+import { NativeBaseProvider } from 'native-base';
 
 export default function App() {
   return (
-    <HooksProvider>
-      <ThemeProvider theme={light}>
-        <StatusBar style="auto" />
-        <Routes />
-      </ThemeProvider>
-    </HooksProvider>
+    <NativeBaseProvider>
+      <HooksProvider>
+        <ThemeProvider theme={light}>
+          <StatusBar style="auto" />
+          <Routes />
+        </ThemeProvider>
+      </HooksProvider>
+    </NativeBaseProvider>
   );
 }
