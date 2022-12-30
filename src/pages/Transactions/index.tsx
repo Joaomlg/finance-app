b78030c8b38cbc5ca51de5d7ad51b96364fdedb2
joaomlg <joaomarcos1999jm@gmail.com>
2022-12-30 16:50:53 -0300

    improvement: transactions flatlist items to useCallback

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import moment, { months } from 'moment';
 import { Avatar, Divider, FlatList, HStack, Icon, Select, Spacer, Text, VStack } from 'native-base';
 import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { RefreshControl } from 'react-native';
+import { ListRenderItemInfo, RefreshControl } from 'react-native';
 import usePluggyService from '../../hooks/pluggyService';
 import { Account, Transaction } from '../../services/pluggy';
 import { ItemsAsyncStorageKey } from '../../utils/contants';
@@ -22,6 +22,16 @@ const Transactions: React.FC = () => {
   const pluggyService = usePluggyService();
 
   const previousDate = useRef(moment(0));
+
+  const totalIncomes = transactions.reduce(
+    (total, transaction) => (transaction.type === 'CREDIT' ? total + transaction.amount : total),
+    0,
+  );
+
+  const totalExpenses = transactions.reduce(
+    (total, transaction) => (transaction.type === 'DEBIT' ? total - transaction.amount : total),
+    0,
+  );
 
   const fetchTransactions = useCallback(async () => {
     setIsLoading(true);
@@ -65,7 +75,34 @@ const Transactions: React.FC = () => {
     fetchTransactions();
   }, [fetchTransactions]);
 
-  const transactionsListSeparator = (transaction: Transaction, index: number) => {
+  const listHeaderComponent = useCallback(
+    () => (
+      <HStack space={3} marginY={3}>
+        <Select
+          flexGrow={1}
+          selectedValue={selectedMonth}
+          borderRadius={100}
+          onValueChange={(value) => setSelectedMonth(value)}
+          fontSize="md"
+        >
+          {months().map((month, index) => (
+            <Select.Item key={index} label={capitalize(month)} value={(index + 1).toString()} />
+          ))}
+        </Select>
+        <VStack>
+          <Text>Renda</Text>
+          <Text>R$ {formatMoney({ value: totalIncomes })}</Text>
+        </VStack>
+        <VStack>
+          <Text>Gasto</Text>
+          <Text>R$ {formatMoney({ value: totalExpenses })}</Text>
+        </VStack>
+      </HStack>
+    ),
+    [selectedMonth, totalExpenses, totalIncomes],
+  );
+
+  const transactionsListSeparator = useCallback((transaction: Transaction, index: number) => {
     const date = moment(transaction.date).startOf('day');
 
     const component =
@@ -88,75 +125,50 @@ const Transactions: React.FC = () => {
     previousDate.current = date;
 
     return component;
-  };
+  }, []);
 
-  const totalIncomes = transactions.reduce(
-    (total, transaction) => (transaction.type === 'CREDIT' ? total + transaction.amount : total),
-    0,
+  const renderItem = useCallback(
+    ({ item, index }: ListRenderItemInfo<Transaction>) => (
+      <>
+        {transactionsListSeparator(item, index)}
+        <HStack paddingY={3} alignItems="center" space={3}>
+          <Avatar
+            backgroundColor="transparent"
+            borderStyle="solid"
+            borderColor="coolGray.500"
+            borderWidth="1"
+          >
+            <Icon
+              as={MaterialIcons}
+              name={item.type == 'DEBIT' ? 'shopping-cart' : 'attach-money'}
+              size="md"
+            />
+          </Avatar>
+          <VStack flexShrink={1}>
+            {item.category && <Text fontWeight="thin">{item.category}</Text>}
+            <Text isTruncated>{item.description}</Text>
+          </VStack>
+          <Spacer />
+          <Text fontWeight="bold">
+            {item.type === 'DEBIT' ? '-' : ''}R${' '}
+            {formatMoney({ value: item.amount, absolute: true })}
+          </Text>
+        </HStack>
+      </>
+    ),
+    [transactionsListSeparator],
   );
 
-  const totalExpenses = transactions.reduce(
-    (total, transaction) => (transaction.type === 'DEBIT' ? total - transaction.amount : total),
-    0,
-  );
+  const keyExtractor = useCallback((item: Transaction) => item.id, []);
 
   return (
     <Container>
       <FlatList
         refreshControl={<RefreshControl refreshing={isLoading} onRefresh={fetchTransactions} />}
-        ListHeaderComponent={() => (
-          <HStack space={3} marginY={3}>
-            <Select
-              flexGrow={1}
-              selectedValue={selectedMonth}
-              borderRadius={100}
-              onValueChange={(value) => setSelectedMonth(value)}
-              fontSize="md"
-            >
-              {months().map((month, index) => (
-                <Select.Item key={index} label={capitalize(month)} value={(index + 1).toString()} />
-              ))}
-            </Select>
-            <VStack>
-              <Text>Renda</Text>
-              <Text>R$ {formatMoney({ value: totalIncomes })}</Text>
-            </VStack>
-            <VStack>
-              <Text>Gasto</Text>
-              <Text>R$ {formatMoney({ value: totalExpenses })}</Text>
-            </VStack>
-          </HStack>
-        )}
+        ListHeaderComponent={listHeaderComponent}
         data={transactions}
-        renderItem={({ item, index }) => (
-          <>
-            {transactionsListSeparator(item, index)}
-            <HStack paddingY={3} alignItems="center" space={3}>
-              <Avatar
-                backgroundColor="transparent"
-                borderStyle="solid"
-                borderColor="coolGray.500"
-                borderWidth="1"
-              >
-                <Icon
-                  as={MaterialIcons}
-                  name={item.type == 'DEBIT' ? 'shopping-cart' : 'attach-money'}
-                  size="md"
-                />
-              </Avatar>
-              <VStack flexShrink={1}>
-                {item.category && <Text fontWeight="thin">{item.category}</Text>}
-                <Text isTruncated>{item.description}</Text>
-              </VStack>
-              <Spacer />
-              <Text fontWeight="bold">
-                {item.type === 'DEBIT' ? '-' : ''}R${' '}
-                {formatMoney({ value: item.amount, absolute: true })}
-              </Text>
-            </HStack>
-          </>
-        )}
-        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
         paddingX={3}
       />
     </Container>
