ff130d5832a4d8db32b0a2ea037a7214f7cc30a1
joaomlg <joaomarcos1999jm@gmail.com>
2023-01-20 16:13:11 -0300

    chore: native-base removed from connections page

--- src/pages/Connections/index.tsx
+++ src/pages/Connections/index.tsx
@@ -2,32 +2,27 @@ import { MaterialIcons } from '@expo/vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useFocusEffect, useNavigation } from '@react-navigation/native';
 import React, { useCallback, useState } from 'react';
-import { Alert, FlatList, RefreshControl, TouchableOpacity } from 'react-native';
+import { Alert, FlatList, ListRenderItemInfo, RefreshControl, Text } from 'react-native';
 import usePluggyService from '../../hooks/pluggyService';
 import { Item, ItemStatus } from '../../services/pluggy';
 import { ItemsAsyncStorageKey } from '../../utils/contants';
 
 import moment from 'moment';
-import {
-  Actionsheet,
-  Avatar,
-  Badge,
-  Box,
-  Fab,
-  HStack,
-  Icon,
-  Spacer,
-  Text,
-  VStack,
-} from 'native-base';
 import { SvgWithCssUri } from 'react-native-svg';
-import { Container } from './styles';
+import {
+  Container,
+  CreatedAtText,
+  FabButton,
+  FabText,
+  ItemContent,
+  ListItem,
+  ListItemAvatar,
+  StatusChip,
+} from './styles';
 
 const Connections: React.FC = () => {
   const [items, setItems] = useState([] as Item[]);
   const [isLoading, setIsLoading] = useState(true);
-  const [selectedItemId, setSelectedItemId] = useState('');
-  const [isActionSheetOpened, setActionSheetOpened] = useState(false);
 
   const navigation = useNavigation();
   const pluggyService = usePluggyService();
@@ -51,116 +46,115 @@ const Connections: React.FC = () => {
     }, [fetchItems]),
   );
 
-  const handleItemPressed = (id: string) => {
-    setSelectedItemId(id);
-    setActionSheetOpened(true);
-  };
+  const handleDeleteItem = useCallback(
+    async (id: string) => {
+      Alert.alert('Apagar conexão?', 'Tem certeza que deseja apagar a conexão?', [
+        { text: 'Cancelar', onPress: () => {} },
+        {
+          text: 'Apagar',
+          onPress: async () => {
+            try {
+              await pluggyService.deleteItem(id);
 
-  const handleActionSheetClosed = () => {
-    setActionSheetOpened(false);
-  };
+              const previousItemsId: string[] = JSON.parse(
+                (await AsyncStorage.getItem(ItemsAsyncStorageKey)) || '[]',
+              );
 
-  const handleDeleteItem = async () => {
-    Alert.alert('Apagar conexão?', 'Tem certeza que deseja apagar a conexão?', [
-      { text: 'Cancelar', onPress: () => {} },
-      { text: 'Apagar', onPress: async () => await tryDeleteItem(selectedItemId) },
-    ]);
-  };
+              const newItemsId = previousItemsId.filter((itemId) => itemId !== id);
 
-  const tryDeleteItem = async (id: string) => {
-    try {
-      await pluggyService.deleteItem(id);
+              await AsyncStorage.setItem(ItemsAsyncStorageKey, JSON.stringify(newItemsId));
 
-      const previousItemsId: string[] = JSON.parse(
-        (await AsyncStorage.getItem(ItemsAsyncStorageKey)) || '[]',
-      );
-
-      const newItemsId = previousItemsId.filter((itemId) => itemId !== id);
-
-      await AsyncStorage.setItem(ItemsAsyncStorageKey, JSON.stringify(newItemsId));
-
-      const newItems = items.filter((item) => item.id !== id);
-      setItems(newItems);
-
-      setActionSheetOpened(false);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+              const newItems = items.filter((item) => item.id !== id);
+              setItems(newItems);
+            } catch (error) {
+              console.error(error);
+            }
+          },
+        },
+      ]);
+    },
+    [pluggyService, items],
+  );
 
   const getItemStatusBadge = (status: ItemStatus) => {
     switch (status) {
       case 'UPDATED':
-        return <Badge colorScheme="success">Atualizado</Badge>;
+        return (
+          <StatusChip backgroundColor="#dcfce7" color="#14532d">
+            Atualizado
+          </StatusChip>
+        );
       case 'LOGIN_ERROR':
-        return <Badge colorScheme="danger">Erro</Badge>;
+        return (
+          <StatusChip backgroundColor="#fee2e2" color="#7f1d1d">
+            Erro
+          </StatusChip>
+        );
       case 'WAITING_USER_INPUT':
-        return <Badge colorScheme="warning">MFA</Badge>;
+        return (
+          <StatusChip backgroundColor="#ffedd5" color="#7c2d12">
+            MFA
+          </StatusChip>
+        );
       case 'OUTDATED':
-        return <Badge colorScheme="dark">Desatualizado</Badge>;
+        return (
+          <StatusChip backgroundColor="#f5f5f4" color="#1c1917">
+            Desatualizado
+          </StatusChip>
+        );
       case 'UPDATING':
-        return <Badge colorScheme="warning">Atualizando</Badge>;
+        return (
+          <StatusChip backgroundColor="#ffedd5" color="#7c2d12">
+            Atualizando
+          </StatusChip>
+        );
     }
   };
+
+  const renderItem = useCallback(
+    ({ item }: ListRenderItemInfo<Item>) => (
+      <ListItem onPress={() => handleDeleteItem(item.id)}>
+        <ListItemAvatar color={'#' + item.connector.primaryColor}>
+          <SvgWithCssUri height="100%" width="100%" uri={item.connector.imageUrl} />
+        </ListItemAvatar>
+        <ItemContent>
+          <Text>{item.connector.name}</Text>
+          <CreatedAtText>
+            Criado em: {moment(item.createdAt).format('DD/MM/YYYY hh:mm')}
+          </CreatedAtText>
+        </ItemContent>
+        {getItemStatusBadge(item.status)}
+      </ListItem>
+    ),
+    [handleDeleteItem],
+  );
 
   return (
     <Container>
       <FlatList
         refreshControl={<RefreshControl refreshing={isLoading} onRefresh={fetchItems} />}
         data={items}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => handleItemPressed(item.id)}>
-            <Box borderWidth="1" borderColor="coolGray.200" borderRadius="lg" marginBottom={3}>
-              <HStack padding={4} space={3} alignItems="center">
-                <Avatar
-                  backgroundColor="transparent"
-                  borderWidth="1"
-                  borderColor={'#' + item.connector.primaryColor}
-                >
-                  <SvgWithCssUri height="100%" width="100%" uri={item.connector.imageUrl} />
-                </Avatar>
-                <VStack>
-                  <Text isTruncated maxWidth="150">
-                    {item.connector.name}
-                  </Text>
-                  <Text fontWeight="thin">
-                    Criado em: {moment(item.createdAt).format('DD/MM/YYYY hh:mm')}
-                  </Text>
-                </VStack>
-                <Spacer />
-                {getItemStatusBadge(item.status)}
-              </HStack>
-            </Box>
-          </TouchableOpacity>
-        )}
+        renderItem={renderItem}
         keyExtractor={(item) => item.id}
         style={{ flex: 1 }}
       />
-      <Actionsheet isOpen={isActionSheetOpened} onClose={handleActionSheetClosed} size="full">
-        <Actionsheet.Content>
-          <Box px={4}>
-            <Text fontSize="16" color="gray.500">
-              Opções
-            </Text>
-          </Box>
-          <Actionsheet.Item disabled startIcon={<Icon as={MaterialIcons} size="6" name="edit" />}>
-            Editar
-          </Actionsheet.Item>
-          <Actionsheet.Item
-            startIcon={<Icon as={MaterialIcons} size="6" name="delete" />}
-            onPress={handleDeleteItem}
-          >
-            Apagar
-          </Actionsheet.Item>
-        </Actionsheet.Content>
-      </Actionsheet>
-      <Fab
-        renderInPortal={false}
-        size="lg"
-        icon={<Icon name="add-circle-outline" as={MaterialIcons} color="white" />}
-        label="Nova conexão"
+      <FabButton
+        style={{
+          shadowColor: '#000',
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 4,
+          elevation: 5,
+        }}
+        activeOpacity={0.6}
         onPress={() => navigation.navigate('connect')}
-      />
+      >
+        <MaterialIcons name="add-circle-outline" color="white" size={20} />
+        <FabText>Nova conexão</FabText>
+      </FabButton>
     </Container>
   );
 };
--- src/pages/Connections/styles.ts
+++ src/pages/Connections/styles.ts
@@ -7,12 +7,55 @@ export const Container = styled.View`
   padding: 24px 12px;
 `;
 
-export const LoadingContainer = styled.View`
-  flex: 1;
-  justify-content: center;
+export const ListItem = styled.TouchableOpacity`
+  border-width: 1px;
+  border-color: #e5e7eb; // coolGray.200
+  border-radius: 8px;
+  padding: 16px;
+  margin-bottom: 12px;
+  flex-direction: row;
   align-items: center;
 `;
 
-export const ItemAvatar = styled(SvgUri)`
+export const ListItemAvatar = styled.View<{ color: string }>`
+  border-style: solid;
+  border-width: 1px;
+  border-color: ${(props) => props.color};
+  border-radius: 100px;
   margin-right: 12px;
+  height: 48px;
+  width: 48px;
+`;
+
+export const ItemContent = styled.View`
+  flex: 1;
+`;
+
+export const CreatedAtText = styled.Text`
+  font-weight: 100;
+`;
+
+export const StatusChip = styled.Text<{ backgroundColor: string; color: string }>`
+  background-color: ${(props) => props.backgroundColor};
+  color: ${(props) => props.color};
+  padding: 4px 8px;
+  font-size: 12px;
+  font-weight: bold;
+`;
+
+export const FabButton = styled.TouchableOpacity`
+  background-color: gray;
+  padding: 16px;
+  position: absolute;
+  right: 16px;
+  bottom: 16px;
+  flex-direction: row;
+  align-items: center;
+  border-radius: 100px;
+`;
+
+export const FabText = styled.Text`
+  color: white;
+  margin-left: 8px;
+  font-size: 16px;
 `;
