c9482850c2cd0749ee59c3dbf2af8afced6a57ab
joaomlg <joaomarcos1999jm@gmail.com>
2023-01-30 22:07:15 -0300

    fix: 'px' added as suffix in common text size

--- src/theme/common.ts
+++ src/theme/common.ts
@@ -1,10 +1,10 @@
 export default {
   TEXT: {
-    HEADING: 24,
-    TITLE: 16,
-    DEFAULT: 14,
-    SMALL: 12,
-    EXTRA_SMALL: 10,
+    HEADING: '24px',
+    TITLE: '16px',
+    DEFAULT: '14px',
+    SMALL: '12px',
+    EXTRA_SMALL: '10px',
   },
   FONT: {
     LIGHT: 'Inter_300Light',
