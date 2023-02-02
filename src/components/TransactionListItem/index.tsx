1090238102e6888df02d9a37c1e435a71d59d828
joaomlg <joaomarcos1999jm@gmail.com>
2023-02-02 19:30:41 -0300

    feat: Transaction list component

---
+++ src/components/TransactionListItem/index.tsx
@@ -0,0 +1,44 @@
+import { MaterialIcons } from '@expo/vector-icons';
+import React from 'react';
+import { ViewProps } from 'react-native';
+import { useTheme } from 'styled-components/native';
+import { Transaction } from '../../services/pluggy';
+import Money from '../Money';
+import Text from '../Text';
+
+import { ListItem, ListItemAmount, ListItemContent } from './styles';
+
+export interface TransactionListItemProps extends ViewProps {
+  item: Transaction;
+}
+
+const TransactionListItem: React.FC<TransactionListItemProps> = ({ item, ...viewProps }) => {
+  const theme = useTheme();
+
+  const value = item.type === 'DEBIT' && item.amount > 0 ? -1 * item.amount : item.amount;
+
+  return (
+    <ListItem {...viewProps}>
+      <MaterialIcons
+        name={item.type === 'DEBIT' ? 'shopping-cart' : 'attach-money'}
+        size={28}
+        color={item.type === 'DEBIT' ? theme.colors.expense : theme.colors.income}
+      />
+      <ListItemContent>
+        {item.category && (
+          <Text variant="extra-light" color="text_light">
+            {item.category}
+          </Text>
+        )}
+        <Text numberOfLines={2} ellipsizeMode="tail">
+          {item.description}
+        </Text>
+      </ListItemContent>
+      <ListItemAmount>
+        <Money variant="default-bold" value={value} />
+      </ListItemAmount>
+    </ListItem>
+  );
+};
+
+export default TransactionListItem;
---
+++ src/components/TransactionListItem/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+export const ListItem = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ListItemContent = styled.View`
+  flex-shrink: 1;
+  margin-left: 16px;
+  margin-right: 16px;
+`;
+
+export const ListItemAmount = styled.View`
+  flex-grow: 1;
+  align-items: flex-end;
+`;
