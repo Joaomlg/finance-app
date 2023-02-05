bbb8383d104962635dd07711b458db2e34afa49a
joaomlg <joaomarcos1999jm@gmail.com>
2023-02-05 18:21:46 -0300

    fix: divider component props

--- src/components/Divider/index.tsx
+++ src/components/Divider/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { ViewProps } from 'react-native';
 import { Container } from './styles';
 
-const Divider: React.FC = () => {
-  return <Container />;
+const Divider: React.FC<ViewProps> = ({ ...props }) => {
+  return <Container {...props} />;
 };
 
 export default Divider;
