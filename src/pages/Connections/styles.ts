aee6b81d85f7be7e5dc05f4afa5c50f374cb5d44
joaomlg <joaomarcos1999jm@gmail.com>
2023-02-08 18:48:56 -0300

    change: connections bottom sheet top radius removed

--- src/pages/Connections/styles.ts
+++ src/pages/Connections/styles.ts
@@ -9,8 +9,6 @@ export const StyledHeader = styled(Header)`
 
 export const BottomSheet = styled(FlexContainer).attrs({ gap: 24 })`
   background-color: ${({ theme }) => theme.colors.backgroundWhite};
-  border-top-left-radius: 24px;
-  border-top-right-radius: 24px;
   padding: 24px;
   flex-grow: 1;
 `;
