39dd8c4301c2b55d6db60fb94e2c06c7966c1882
joaomlg <joaomarcos1999jm@gmail.com>
2023-02-08 18:53:37 -0300

    feat: transactions screen improved

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,147 +1,62 @@
-import { MaterialIcons } from '@expo/vector-icons';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import moment, { Moment } from 'moment';
-import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import { FlatList, ListRenderItemInfo, RefreshControl, Text } from 'react-native';
-import MonthYearPicker from '../../components/MonthYearPicker';
-import usePluggyService from '../../hooks/pluggyService';
-import { Account, Transaction } from '../../services/pluggy';
-import { ItemsAsyncStorageKey } from '../../utils/contants';
-import { formatMoney } from '../../utils/money';
-
+import moment from 'moment';
+import React, { useCallback, useContext, useRef } from 'react';
+import { ListRenderItemInfo, RefreshControl } from 'react-native';
+import { useTheme } from 'styled-components/native';
+import Divider from '../../components/Divider';
+import Money from '../../components/Money';
+import ScreenContainer from '../../components/ScreenContainer';
+import Text from '../../components/Text';
+import AppContext from '../../contexts/AppContext';
+import { Transaction } from '../../services/pluggy';
 import {
-  Container,
-  Divider,
-  ListHeader,
-  ListItem,
-  ListItemAmount,
-  ListItemAmountValue,
-  ListItemAvatar,
-  ListItemCategory,
-  ListItemContent,
-  ListItemLabel,
+  ListHeaderContainer,
   ListSeparatorContainer,
   ListSeparatorDate,
-  ListSeparatorDateDay,
-  ListSeparatorDateMonth,
-  MonthSelectorButton,
-  MonthSelectorButtonText,
-  MonthSelectorContainer,
-  TotalInfo,
+  StyledHeader,
+  StyledTransactionListItem,
+  TransactionList,
 } from './styles';
 
-const NUBANK_IGNORED_TRANSACTIONS = [
-  'Pagamento da fatura',
-  'Pagamento recebido',
-  'Dinheiro guardado',
-  'Dinheiro resgatado',
-];
+const Transactions2: React.FC = () => {
+  const {
+    isLoading,
+    hideValues,
+    setHideValues,
+    transactions,
+    fetchTransactions,
+    date,
+    totalIncomes,
+    totalExpenses,
+  } = useContext(AppContext);
 
-const now = moment();
+  const theme = useTheme();
 
-const Transactions: React.FC = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [transactions, setTransactions] = useState([] as Transaction[]);
-  const [selectedMonth, setSelectedMonth] = useState(now);
-  const [monthYearPickerOpened, setMonthYearPickerOpened] = useState(false);
+  const ItemDividerPreviousDateRef = useRef(moment(0));
 
-  const pluggyService = usePluggyService();
-
-  const previousDate = useRef(moment(0));
-
-  const totalIncomes = useMemo(
-    () =>
-      transactions.reduce(
-        (total, transaction) =>
-          transaction.type === 'CREDIT' ? total + Math.abs(transaction.amount) : total,
-        0,
-      ),
-    [transactions],
-  );
-
-  const totalExpenses = useMemo(
-    () =>
-      transactions.reduce(
-        (total, transaction) =>
-          transaction.type === 'DEBIT' ? total + Math.abs(transaction.amount) : total,
-        0,
-      ),
-    [transactions],
-  );
-
-  const fetchTransactions = useCallback(async () => {
-    setIsLoading(true);
-
-    const itemsId: string[] = JSON.parse(
-      (await AsyncStorage.getItem(ItemsAsyncStorageKey)) || '[]',
+  const renderHeaderComponent = useCallback(() => {
+    return (
+      <ListHeaderContainer>
+        <Text variant="light" color="textLight">
+          {transactions.length} transações
+        </Text>
+        <Money variant="heading" value={totalIncomes - totalExpenses} />
+      </ListHeaderContainer>
     );
-
-    const accountPromiseResults = await Promise.all(
-      itemsId.map((id) => pluggyService.fetchAccounts(id)),
-    );
-
-    const accounts = accountPromiseResults.reduce(
-      (list, item) => [...list, ...item.results],
-      [] as Account[],
-    );
-
-    const startDate = moment(selectedMonth).startOf('month');
-    const endDate = moment(selectedMonth).endOf('month');
-
-    const promiseResults = await Promise.all(
-      accounts.map(({ id }) =>
-        pluggyService.fetchTransactions(id, {
-          pageSize: 500,
-          from: startDate.format('YYYY-MM-DD'),
-          to: endDate.format('YYYY-MM-DD'),
-        }),
-      ),
-    );
-
-    const transactionsList = promiseResults
-      .reduce((list, item) => [...list, ...item.results], [] as Transaction[])
-      .filter((item) => !NUBANK_IGNORED_TRANSACTIONS.includes(item.description))
-      .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
-
-    setTransactions(transactionsList);
-    setIsLoading(false);
-  }, [pluggyService, selectedMonth]);
-
-  useEffect(() => {
-    fetchTransactions();
-  }, [fetchTransactions]);
-
-  const listHeaderComponent = useCallback(
-    () => (
-      <ListHeader>
-        <MonthSelectorContainer>
-          <MonthSelectorButton onPress={() => setMonthYearPickerOpened(true)}>
-            <MonthSelectorButtonText>{selectedMonth.format('MMMM YYYY')}</MonthSelectorButtonText>
-            <MaterialIcons name="expand-more" size={32} color="gray" />
-          </MonthSelectorButton>
-        </MonthSelectorContainer>
-        <TotalInfo>
-          <Text>Renda</Text>
-          <Text>R$ {formatMoney({ value: totalIncomes })}</Text>
-        </TotalInfo>
-        <TotalInfo>
-          <Text>Gasto</Text>
-          <Text>R$ {formatMoney({ value: totalExpenses })}</Text>
-        </TotalInfo>
-      </ListHeader>
-    ),
-    [selectedMonth, totalExpenses, totalIncomes],
-  );
+  }, [transactions, totalIncomes, totalExpenses]);
 
   const renderListItemSeparator = useCallback((transaction: Transaction, index: number) => {
     const date = moment(transaction.date).startOf('day');
 
     const component =
-      index === 0 || date.isBefore(previousDate.current, 'day') ? (
+      index === 0 || date.isBefore(ItemDividerPreviousDateRef.current, 'day') ? (
         <ListSeparatorContainer>
           <ListSeparatorDate>
-            <ListSeparatorDateDay>{date.format('DD')}</ListSeparatorDateDay>
-            <ListSeparatorDateMonth>{date.format('MMM')}</ListSeparatorDateMonth>
+            <Text variant="title" color="textLight">
+              {date.format('DD')}
+            </Text>
+            <Text variant="light" color="textLight">
+              {date.format('MMM')}
+            </Text>
           </ListSeparatorDate>
           <Divider />
         </ListSeparatorContainer>
@@ -149,65 +64,49 @@ const Transactions: React.FC = () => {
         <></>
       );
 
-    previousDate.current = date;
+    ItemDividerPreviousDateRef.current = date;
 
     return component;
   }, []);
 
   const renderItem = useCallback(
-    ({ item, index }: ListRenderItemInfo<Transaction>) => (
-      <>
-        {renderListItemSeparator(item, index)}
-        <ListItem>
-          <ListItemAvatar>
-            <MaterialIcons
-              name={item.type == 'DEBIT' ? 'shopping-cart' : 'attach-money'}
-              size={24}
-              color="gray"
-            />
-          </ListItemAvatar>
-          <ListItemContent>
-            {item.category && <ListItemCategory>{item.category}</ListItemCategory>}
-            <ListItemLabel numberOfLines={1} ellipsizeMode="tail">
-              {item.description}
-            </ListItemLabel>
-          </ListItemContent>
-          <ListItemAmount>
-            <ListItemAmountValue>
-              {item.type === 'DEBIT' ? '-' : ''}R${' '}
-              {formatMoney({ value: item.amount, absolute: true })}
-            </ListItemAmountValue>
-          </ListItemAmount>
-        </ListItem>
-      </>
-    ),
+    ({ item, index }: ListRenderItemInfo<Transaction>) => {
+      return (
+        <>
+          {renderListItemSeparator(item, index)}
+          <StyledTransactionListItem key={index} item={item} />
+        </>
+      );
+    },
     [renderListItemSeparator],
   );
 
-  const keyExtractor = useCallback((item: Transaction) => item.id, []);
-
-  const handleMonthYearPickerChange = (value: Moment) => {
-    setSelectedMonth(value);
-    setMonthYearPickerOpened(false);
-  };
-
   return (
-    <Container>
-      <FlatList
-        refreshControl={<RefreshControl refreshing={isLoading} onRefresh={fetchTransactions} />}
-        ListHeaderComponent={listHeaderComponent}
+    <ScreenContainer>
+      <StyledHeader
+        title={date.format('MMMM')}
+        actions={[
+          {
+            icon: hideValues ? 'visibility-off' : 'visibility',
+            onPress: () => setHideValues(!hideValues),
+          },
+        ]}
+      />
+      <TransactionList
+        refreshControl={
+          <RefreshControl
+            refreshing={isLoading}
+            onRefresh={fetchTransactions}
+            colors={[theme.colors.primary]}
+          />
+        }
         data={transactions}
         renderItem={renderItem}
-        keyExtractor={keyExtractor}
-        style={{ paddingHorizontal: 12 }}
+        keyExtractor={(item) => item.id}
+        ListHeaderComponent={renderHeaderComponent}
       />
-      <MonthYearPicker
-        isOpen={monthYearPickerOpened}
-        onChange={(value) => handleMonthYearPickerChange(value)}
-        onClose={() => setMonthYearPickerOpened(false)}
-      />
-    </Container>
+    </ScreenContainer>
   );
 };
 
-export default Transactions;
+export default Transactions2;
--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -1,39 +1,30 @@
+import { FlatList, FlatListProps } from 'react-native';
 import styled from 'styled-components/native';
+import FlexContainer from '../../components/FlexContainer';
+import Header from '../../components/Header';
+import TransactionListItem from '../../components/TransactionListItem';
+import { Transaction } from '../../services/pluggy';
 
-export const Container = styled.SafeAreaView`
-  flex: 1;
+export const StyledHeader = styled(Header)`
+  padding: 24px;
+  padding-left: 16px;
+`;
+
+export const ListHeaderContainer = styled(FlexContainer).attrs({ gap: 4 })`
+  margin-bottom: 12px;
+`;
+
+export const TransactionList = styled(
+  FlatList as new (props: FlatListProps<Transaction>) => FlatList<Transaction>,
+)`
   background-color: ${({ theme }) => theme.colors.backgroundWhite};
-`;
-
-export const ListHeader = styled.View`
-  flex-direction: row;
-  margin: 12px 0;
-`;
-
-export const MonthSelectorContainer = styled.View`
-  flex-grow: 1;
-`;
-
-export const MonthSelectorButton = styled.TouchableOpacity`
-  flex: 1;
-  flex-direction: row;
-  align-items: center;
-`;
-
-export const MonthSelectorButtonText = styled.Text`
-  text-transform: uppercase;
-  font-weight: bold;
-  font-size: 18px;
-  color: gray;
-`;
-
-export const TotalInfo = styled.View`
-  margin-left: 12px;
+  padding: 24px;
 `;
 
 export const ListSeparatorContainer = styled.View`
   flex-direction: row;
   align-items: center;
+  margin: 12px 0;
 `;
 
 export const ListSeparatorDate = styled.View`
@@ -41,58 +32,6 @@ export const ListSeparatorDate = styled.View`
   margin-right: 12px;
 `;
 
-export const ListSeparatorDateDay = styled.Text`
-  font-weight: bold;
-  font-size: 20px;
-  color: #9ca3af; // coolGray.400
-`;
-
-export const ListSeparatorDateMonth = styled.Text`
-  font-weight: 100;
-  text-transform: uppercase;
-  color: #9ca3af; // coolGray.400
-`;
-
-export const Divider = styled.View`
-  height: 1px;
-  width: 100%;
-  background-color: #e5e7eb; // coolGray.200
-  margin-bottom: 8px;
-`;
-
-export const ListItem = styled.View`
-  flex-direction: row;
-  align-items: center;
+export const StyledTransactionListItem = styled(TransactionListItem)`
   padding: 12px 0;
-`;
-
-export const ListItemAvatar = styled.View`
-  border-style: solid;
-  border-width: 1px;
-  border-color: gray;
-  border-radius: 100px;
-  padding: 8px;
-  margin-right: 12px;
-`;
-
-export const ListItemContent = styled.View`
-  flex-shrink: 1;
-  margin-right: 12px;
-`;
-
-export const ListItemCategory = styled.Text`
-  font-weight: 100;
-  font-size: 10px;
-  text-transform: uppercase;
-`;
-
-export const ListItemLabel = styled.Text``;
-
-export const ListItemAmount = styled.View`
-  flex-grow: 1;
-  align-items: flex-end;
-`;
-
-export const ListItemAmountValue = styled.Text`
-  font-weight: bold;
 `;
