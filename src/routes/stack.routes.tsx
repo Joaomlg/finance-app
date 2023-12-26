475c26bd64579090e9a1f98765cb42d726ba70a8
joaomlg <joaomarcos1999jm@gmail.com>
2023-12-26 19:24:19 -0300

    chore: Connect page turned BelvoConnect

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,6 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import Connect from '../pages/Connect';
+import Connect from '../pages/BelvoConnect';
 import Connections from '../pages/Connections';
 import History from '../pages/History';
 import Home from '../pages/Home';
