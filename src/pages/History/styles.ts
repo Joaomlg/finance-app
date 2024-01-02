5899a3b6f1ec0fd85ab63bffbc29a9bb89f5a527
joaomlg <joaomarcos1999jm@gmail.com>
2024-01-02 15:35:43 -0300

    fix: MonthlyBalance type reference fixed on History page

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -1,10 +1,10 @@
 import { FlatList, FlatListProps } from 'react-native';
 import styled from 'styled-components/native';
-import { MonthBalance } from '.';
 import Divider from '../../components/Divider';
 import FlexContainer from '../../components/FlexContainer';
 import Header from '../../components/Header';
 import HorizontalBar from '../../components/HorizontalBar';
+import { MonthlyBalance } from '../../contexts/AppContext';
 
 export const StyledHeader = styled(Header)`
   padding: 24px;
@@ -12,7 +12,7 @@ export const StyledHeader = styled(Header)`
 `;
 
 export const StyledFlatList = styled(
-  FlatList as new (props: FlatListProps<MonthBalance>) => FlatList<MonthBalance>,
+  FlatList as new (props: FlatListProps<MonthlyBalance>) => FlatList<MonthlyBalance>,
 ).attrs({
   contentContainerStyle: {
     padding: 24,
