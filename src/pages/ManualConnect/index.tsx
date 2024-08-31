3c322739588eb3c292c950762c4e6e2e3e4c6421
joaomlg <joaomarcos1999jm@gmail.com>
2024-08-31 17:19:20 -0300

    fix: manual connect screen fixed

--- src/pages/ManualConnect/index.tsx
+++ src/pages/ManualConnect/index.tsx
@@ -1,24 +1,56 @@
 import { useNavigation } from '@react-navigation/native';
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Toast from 'react-native-toast-message';
-import Button from '../../components/Button';
+import Divider from '../../components/Divider';
+import ListItemSelection from '../../components/Forms/Selection/ListItemSelection';
 import ScreenContainer from '../../components/ScreenContainer';
 import ScreenContent from '../../components/ScreenContent';
+import ScreenFloatingButton from '../../components/ScreenFloatingButton';
 import ScreenHeader from '../../components/ScreenHeader';
-import Text from '../../components/Text';
-import { TextInput } from './styles';
+import TextInput from '../../components/TextInput';
+import AppContext2 from '../../contexts/AppContext2';
+import useBelvoService from '../../hooks/useBelvoService';
+import useBottomSheet from '../../hooks/useBottomSheet';
+import usePluggyService from '../../hooks/usePluggyService';
+import Provider from '../../models/provider';
 
 const ManualConnect: React.FC = () => {
   const [id, setId] = useState('');
+  const [provider, setProvider] = useState<Provider | undefined>();
 
-  const storeConnection = () => {};
-  const deleteConnection = () => {};
+  const { setupConnection } = useContext(AppContext2);
 
+  const { openBottomSheet, closeBottomSheet } = useBottomSheet();
   const navigation = useNavigation();
+  const pluggyService = usePluggyService();
+  const belvoService = useBelvoService();
 
-  const saveConnection = () => {
+  const renderWalletTypeSelector = () => {
+    const handleItemPressed = (provider: Provider) => {
+      setProvider(provider);
+      closeBottomSheet();
+    };
+
+    return (
+      <ListItemSelection
+        title="Provedor"
+        items={[
+          {
+            text: 'Pluggy',
+            onPress: () => handleItemPressed('PLUGGY'),
+          },
+          {
+            text: 'Belvo',
+            onPress: () => handleItemPressed('BELVO'),
+          },
+        ]}
+      />
+    );
+  };
+
+  const saveConnection = async () => {
     try {
-      storeConnection();
+      await setupConnection(id, provider === 'PLUGGY' ? pluggyService : belvoService);
       Toast.show({ type: 'success', text1: 'Conexão adicionada com sucesso!' });
       navigation.navigate('wallets');
     } catch (err) {
@@ -26,33 +58,31 @@ const ManualConnect: React.FC = () => {
     }
   };
 
-  const RemoveConnection = () => {
-    try {
-      deleteConnection();
-      Toast.show({ type: 'success', text1: 'Conexão removida com sucesso!' });
-      navigation.navigate('wallets');
-    } catch (err) {
-      Toast.show({ type: 'error', text1: 'Não foi possível remover a conexão!' });
-    }
-  };
-
   return (
-    <ScreenContainer>
-      <ScreenHeader title="Conexão manual" />
-      <ScreenContent>
-        <TextInput placeholder="Identificador" onChangeText={setId} value={id} />
-        <Button onPress={saveConnection}>
-          <Text typography="title" color="textWhite">
-            Adicionar
-          </Text>
-        </Button>
-        <Button onPress={RemoveConnection}>
-          <Text typography="title" color="textWhite">
-            Remover
-          </Text>
-        </Button>
-      </ScreenContent>
-    </ScreenContainer>
+    <>
+      <ScreenContainer>
+        <ScreenHeader title="Conexão manual" />
+        <ScreenContent>
+          <TextInput
+            iconLeft="font-download"
+            placeholder="Identificador"
+            onChangeText={setId}
+            value={id}
+          />
+          <Divider />
+          <TextInput
+            placeholder="Provedor"
+            iconLeft="hub"
+            iconRight="navigate-next"
+            onPress={() => openBottomSheet(renderWalletTypeSelector())}
+            value={provider}
+            readOnly
+          />
+          <Divider />
+        </ScreenContent>
+      </ScreenContainer>
+      <ScreenFloatingButton icon="check" onPress={saveConnection} />
+    </>
   );
 };
 
