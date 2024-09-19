1ba461bda3851208b8b6fcc3387dbee15011bc86
joaomlg <joaomarcos1999jm@gmail.com>
2024-09-19 14:15:48 -0300

    chore: version changed to 2.1.2

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: IS_DEV ? 'Finans (Dev)' : 'Finans',
     slug: 'finance-app',
-    version: '2.1.1',
+    version: '2.1.2',
     orientation: 'portrait',
     icon: IS_DEV ? './src/assets/dev-icon.png' : './src/assets/icon.png',
     userInterfaceStyle: 'automatic',
