11add5eba0eb07d2ee98c84e4a65e24daff3edd3
joaomlg <joaomarcos1999jm@gmail.com>
2025-01-12 12:00:04 -0300

    feat: adding construction expense category

--- src/utils/category.ts
+++ src/utils/category.ts
@@ -54,7 +54,7 @@ export const expensePresetCategories: Category[] = [
     id: PRESET_CATEGORY_ID_PREFIX + 'expense-3',
     name: 'Compras',
     icon: 'shopping-cart',
-    color: '#ff7f0e',
+    color: '#ffbb78',
     type: 'EXPENSE',
   },
   {
@@ -153,6 +153,13 @@ export const expensePresetCategories: Category[] = [
     name: 'Pagamentos',
     icon: 'payments',
     color: '#bcbd22',
+    type: 'EXPENSE',
+  },
+  {
+    id: PRESET_CATEGORY_ID_PREFIX + 'expense-19',
+    name: 'Manutenção',
+    icon: 'construction',
+    color: '#ff7f0e',
     type: 'EXPENSE',
   },
 ];
