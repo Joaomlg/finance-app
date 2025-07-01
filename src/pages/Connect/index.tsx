cdcfd29a9420a553b7a13d662f80d13a98ba8ad2
João Marcos <joaomarcos1999jm@gmail.com>
2025-06-30 21:44:55 -0300

    feat: using flat list on connect page instead of cards

--- src/pages/Connect/index.tsx
+++ src/pages/Connect/index.tsx
@@ -2,11 +2,12 @@ import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import BelvoLogo from '../../assets/icon/belvo_logo.svg';
 import PluggyLogo from '../../assets/icon/pluggy_logo.svg';
+import Divider from '../../components/Divider';
 import ScreenContainer from '../../components/ScreenContainer';
 import ScreenContent from '../../components/ScreenContent';
 import ScreenHeader from '../../components/ScreenHeader';
 import Text from '../../components/Text';
-import { CustomCard } from './styles';
+import { ProviderItem } from './styles';
 
 const Connect: React.FC = () => {
   const navigation = useNavigation();
@@ -16,13 +17,16 @@ const Connect: React.FC = () => {
       <ScreenHeader title="Connectar" />
       <ScreenContent>
         <Text typography="title">Selecione um provedor</Text>
-        <CustomCard onPress={() => navigation.navigate('connect/pluggy')}>
+        <ProviderItem onPress={() => navigation.navigate('connect/pluggy')}>
           <PluggyLogo height={36} width={130} />
-        </CustomCard>
+        </ProviderItem>
         {__DEV__ && (
-          <CustomCard onPress={() => navigation.navigate('connect/belvo')}>
-            <BelvoLogo height={36} width={100} />
-          </CustomCard>
+          <>
+            <Divider />
+            <ProviderItem onPress={() => navigation.navigate('connect/belvo')}>
+              <BelvoLogo height={36} width={100} />
+            </ProviderItem>
+          </>
         )}
       </ScreenContent>
     </ScreenContainer>
